852867d5688bc996a26994edd0f94f7c66b0638e
Jose Armando Castillo Quiala <josearmandocastilloquiala@Joses-MBP.coworking.lan>
2017-08-03 12:54:30 +0200

    adding the load to a function

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var url = './bands.json';
 
-fetch(url)
-  .then(function(response){
-    console.log(response);
-    return response.json();
-  })
-  .then(function(json){
-    console.info(json[2]);
-  })
-  .catch(function(err){
-    console.info(err);
-  });
+function loadJson(){
+  var url = './bands.json';
+
+  fetch(url)
+    .then(function(response){
+      console.log(response);
+      return response.json();
+    })
+    .then(function(json){
+      console.info(json[2]);
+    })
+    .catch(function(err){
+      console.info(err);
+    });
+}
