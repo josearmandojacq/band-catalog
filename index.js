f3742a6f0122973f088ba5f2462f9a7a9b555281
Jose Armando Castillo Quiala <josearmandocastilloquiala@Joses-MBP.coworking.lan>
2017-08-07 12:53:34 +0200

    showing the members and the albums

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ var mainElement = document.querySelector('main');
 
 function whenJSONLoad(bands){
   var completeListOfBands = bands.map(function(band , i){
-    for(var i = 0; i < band.members.length; i++){
-      console.log(band.members[i].name);
-    }
+  var memberslist = renderMembers(band.members);
+  var albumslist = renderAlbums(band.albums);
     return `<div id="band1">
       <h2 class="space-between">${band.name}<h2>
       <h3 class="space-between">${band.genre}<h3>
       <h3 class="space-between">Members<h3>
 
       <ul class="members space-between">
-        <li></li>
-        <li>John Doe Guitar</li>
+        <li>${memberslist}</li>
+
       </ul>
       <h3 class="space-between">Albums<h3>
 
 
       <ul class="albums space-between">
-        <li>Album Name (2001)</li>
-        <li>Album Name (2001)</li>
+        <li>${albumslist}</li>
       </ul>
     </div>`
 
@@ -42,6 +40,61 @@ function loadJson(url){
     });
 }
 
+function renderMembers(members) {
+  var  htmlString = "<h4>Members</h4><ul>";
+  members.sort(sortByName).forEach(function(member){
+    htmlString += `
+
+          <li>
+            <span class="member-name">${member.name}</span>
+            <span class="member-instrument">${member.instrument}</span>
+          </li>`
+  });
+  return htmlString + "</ul>";
+}
+
+function renderAlbums(albums) {
+  var htmlString = "<h4>Albums</h4><ul>";
+  albums.sort(sortByNumber).forEach(function(album) {
+
+    htmlString += `
+
+          <li>
+            <span class="album-name">${album.title}</span>
+            <span class="album-release-year">${album.releaseYear}</span>
+          </li>`
+  });
+  return htmlString + "</ul>";
+}
+
+function sortByName(a,b) {
+  if (a.name < b.name)
+     return -1;
+  if (a.name > b.name)
+    return 1;
+  return 0;
+}
+
+function sortByNumber(a,b) {
+  return a.releaseYear - b.releaseYear;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////// Calling the functions ////////////////
 
 var url = './bands.json';
 loadJson(url);
